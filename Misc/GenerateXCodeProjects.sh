1c33b7aa23456f72a742396e795dca878ff3b33e
Cem Dervis <cem@dervis.de>
2025-08-21 18:02:47 +0200

    Add GenerateXCodeProjects.sh [skip ci]

---
+++ Misc/GenerateXCodeProjects.sh
@@ -0,0 +1,8 @@
+build_dir=Build/Xcode
+macos_build_dir=$build_dir/macOS
+ios_build_dir=$build_dir/iOS
+
+cmake -B $macos_build_dir -G Xcode -DCPM_SOURCE_CACHE=Build
+cmake -B $ios_build_dir -G Xcode -DCMAKE_SYSTEM_NAME=iOS -DCPM_SOURCE_CACHE=Build
+
+
