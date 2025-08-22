f4b34731f7caa109f4e1b5859884ac686a2cf8e8
Cem Dervis <cem@dervis.de>
2025-08-22 13:02:17 +0200

    11 minimize dependency download sizes (#44)

--- Misc/DownloadDepFile.cmake
+++ Misc/DownloadDepFile.cmake
@@ -1,12 +1,12 @@
 function(polly_download_dep_file name url expected_hash)
     set(filename ${polly_deps_headers_dir}/${name})
     set(checksum "")
-    
-    polly_log("filename: ${filename}")
 
     if (EXISTS ${filename})
         file(SHA256 ${filename} checksum)
     endif ()
+    
+    set(checksum "SHA256=${checksum}")
 
     if (NOT checksum STREQUAL ${expected_hash})
         polly_log("Fetching ${name}")
