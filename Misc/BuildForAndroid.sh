72d058d9678036939a66e53aba5508e003e1b2fd
Cem Dervis <cem@dervis.de>
2025-09-07 16:50:56 +0200

    Improve Android build script

--- Misc/BuildForAndroid.sh
+++ Misc/BuildForAndroid.sh
@@ -1,19 +1,34 @@
 # Script expects the CMake build configuration to be passed as an argument,
 # i.e. "Debug" or "Release".
 
-ndk=28.0.13004108
-build_dir=Build/Android/$1
+android_sdk=$ANDROID_SDK
+android_cmake_binary=$android_sdk/cmake/3.22.1/bin/cmake
+android_ndk_version=29.0.14033849
+android_platform=android-28
+android_stl=c++_shared
+android_toolchain=$android_sdk/ndk/$android_ndk_version/build/cmake/android.toolchain.cmake
 
-echo Building with Android NDK: $ndk
-echo Directory: $build_dir
-echo Configuration type: $1
+echo Building with Android NDK: $android_ndk_version
+echo Android Platform: $android_platform
 
-cmake -B $build_dir \
-      -DCMAKE_TOOLCHAIN_FILE=$ANDROID_SDK/ndk/$ndk/build/cmake/android.toolchain.cmake \
-      -G Ninja \
-      -DCPM_SOURCE_CACHE=Build \
-      -DCMAKE_BUILD_TYPE=$1 \
-      -DPOLLY_BUILD_APPS=OFF
+for android_abi in arm64-v8a armeabi-v7a x86_64
+do
+    config=Debug
 
-cmake --build $build_dir --parallel --target Polly --config $1
+    echo Building for $android_abi-$config
+    build_dir=Build/Android/$android_abi-$config
+    
+    $android_cmake_binary \
+        -B $build_dir \
+        -G Ninja \
+        -D CPM_SOURCE_CACHE=Build \
+        -D CMAKE_BUILD_TYPE=$config \
+        -D CMAKE_TOOLCHAIN_FILE=$android_toolchain \
+        -D ANDROID_ABI=$android_abi \
+        -D ANDROID_PLATFORM=$android_platform \
+        -D ANDROID_STL=$android_stl \
+        -D POLLY_BUILD_APPS=OFF
+
+    $android_cmake_binary --build $build_dir --target Polly --parallel || { echo 'Build failed'; exit 1; }
+done
 
